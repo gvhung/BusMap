2ad5b461a873130257f1caeedebbc89c12c87fa6
PSzymon <pol.szym95@gmail.com>
2018-08-31 12:44:54 +0200

    Maps configurated and working

--- BusMap.Mobile/BusMap.Mobile/Views/MainPage.xaml.cs
+++ BusMap.Mobile/BusMap.Mobile/Views/MainPage.xaml.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
-namespace BusMap.Mobile
+namespace BusMap.Mobile.Views
 {
     public partial class MainPage : ContentPage
     {
