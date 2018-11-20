e145701133d603fecefd3aa7e513e00da3fe6c2f
PSzymon <pol.szym95@gmail.com>
2018-11-20 22:30:38 +0100

    Displaying color depending of punctuality % on RoutesListPage
    Using trigger and ValueConverter

---
+++ BusMap.Mobile/BusMap.Mobile/EventArgsConverters/PunctualityPercentageConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace BusMap.Mobile.EventArgsConverters
+{
+    internal class PunctualityPercentageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int result;
+            var valueString = value as string;
+            valueString = valueString.Substring(0, 2);
+
+            int.TryParse(valueString, out result);
+            if (result > 49)
+                return true;
+            else
+                return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
