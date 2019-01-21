7a41b8e8283568f66178479fff8f3bfbfdc30405
PSzymon <pol.szym95@gmail.com>
2019-01-21 22:27:15 +0100

    Disabling tracking button when route is not is hours range

--- BusMap.Mobile/BusMap.Mobile/ViewModels/TraceTrackingPageViewModel.cs
+++ BusMap.Mobile/BusMap.Mobile/ViewModels/TraceTrackingPageViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,11 +22,14 @@ namespace BusMap.Mobile.ViewModels
         private readonly IDataService _dataService;
         private readonly IGeolocationBackgroundService _geolocationBackgroundService;
 
+        private int _lastBusStopId;
+
         private Route _route;
         private bool _isTrackingStarted;
         private string _testLabelText;
 
         private List<BusStop> _busStops;
+        private bool _isTrackingEnabled = true;
 
         public Route Route
         {
@@ -34,6 +38,7 @@ namespace BusMap.Mobile.ViewModels
             {
                 SetProperty(ref _route, value);
                 _busStops.AddRange(_route.BusStops);
+                _lastBusStopId = _busStops.Last().Id;
             }
         }
 
@@ -47,6 +52,12 @@ namespace BusMap.Mobile.ViewModels
         {
             get => _testLabelText;
             set => SetProperty(ref _testLabelText, value);
+        }
+
+        public bool IsTrackingEnabled
+        {
+            get => _isTrackingEnabled;
+            set => SetProperty(ref _isTrackingEnabled, value);
         }
 
 
@@ -63,6 +74,8 @@ namespace BusMap.Mobile.ViewModels
 
         public ICommand StartTrackingCommand => new DelegateCommand(async () =>
         {
+            
+
             if (!IsTrackingStarted)
             {
                 IsTrackingStarted = true;
@@ -108,6 +121,16 @@ namespace BusMap.Mobile.ViewModels
                             BusStopId = busStop.Id,
                             Hour = new TimeSpan(currentHour.Hour, currentHour.Minute, 0)
                         });
+                        MessagingHelper.Toast($"Trace added: {busStop.Address}, {busStop.Label}", ToastTime.LongTime);
+
+                        if (busStop.Id == _lastBusStopId)
+                        {
+                            CrossGeolocator.Current.PositionChanged -= GeolocatorOnPositionChanged;
+                            _geolocationBackgroundService.StopServiceAsync().Wait();
+                            IsTrackingStarted = false;
+                            MessagingHelper.Toast($"Last bus stop arrived. Tracking ended.", ToastTime.LongTime);
+                        }
+
                         return;
                     }
 
@@ -124,6 +147,17 @@ namespace BusMap.Mobile.ViewModels
                 Route = parameters["route"] as Route;
                 Title = $"New Track for {Route.Name}";
             }
+
+            var routeStartingTime = Route.BusStops.First().Hour;
+            var routeEndingTime = Route.BusStops.Last().Hour;
+            var currentTime = DateTime.Now.TimeOfDay;
+
+            if (currentTime < routeStartingTime.Subtract(new TimeSpan(1, 0, 0))
+                || currentTime > routeEndingTime.Add(new TimeSpan(1, 0, 0)))
+            {
+                IsTrackingEnabled = false;
+            }
+                
         }
 
         public override async void OnNavigatedFrom(NavigationParameters parameters)
